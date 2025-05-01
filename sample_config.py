5c0869188af00ecd062bb6965fcf159d1d7ef71a
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-05-01 20:11:47 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -73,7 +73,7 @@ class Config(object):
     CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
     ZELZAL_Z = int(2581535293)
     PING_TEXT = os.environ.get("PING_TEXT", None)
-    ZELZAL_A = int(os.environ.get("ZELZAL_A") or -1002581535293)
+    ZELZAL_A = os.environ.get("ZELZAL_A", "@mplyqa_py")
     ZELZAL_HASH = int(os.environ.get("ZELZAL_HASH") or -4786013969211250263)
     ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
     BOT_PIC = os.environ.get("BOT_PIC", None)
